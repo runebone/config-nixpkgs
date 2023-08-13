0fe170e33bdceacc9859af49e665fee713a5456f
Konstantin Runov <runebone1@gmail.com>
2023-08-13 17:56:25 +0300

    use gruvbox-material; comment out highlight

--- neovim/plugin/colors.lua
+++ neovim/plugin/colors.lua
@@ -1,12 +1,14 @@
 require('rose-pine').setup({ disable_background = true })
 
 function ColorMyPencils(color) 
-	color = color or "gruvbox"
+	color = color or "gruvbox-material"
 
 	vim.cmd.colorscheme(color)
 
+    disable_background()
+
     -- Highlight comments using treesitter comment parser
-    vim.api.nvim_set_hl(0, "@text.note", { link = "Search" })
+    -- vim.api.nvim_set_hl(0, "@text.note", { link = "Search" })
 end
 
 ColorMyPencils()
