c901bf458c416dbda8cc8bda23157acbb313f907
Konstantin Runov <runebone1@gmail.com>
2023-08-13 15:09:03 +0300

    don't remove background

--- neovim/plugin/colors.lua
+++ neovim/plugin/colors.lua
@@ -2,10 +2,8 @@ require('rose-pine').setup({ disable_background = true })
 
 function ColorMyPencils(color) 
 	color = color or "gruvbox"
-	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.cmd.colorscheme(color)
 
     -- Highlight comments using treesitter comment parser
     vim.api.nvim_set_hl(0, "@text.note", { link = "Search" })
